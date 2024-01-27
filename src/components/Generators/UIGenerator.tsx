443022d4163ba1cea6fe7e4a8684544d7cc18ec1
PiotrPrzybyla <pepapek@gmail.com>
2024-01-27 12:21:25 +0100

    Dark theme

--- src/components/Generators/UIGenerator.tsx
+++ src/components/Generators/UIGenerator.tsx
@@ -1,17 +1,21 @@
-import React, { ReactNode } from 'react'
-import Navigation from '../Navigation/Navigation'
+import React, { ReactNode } from "react";
+import Navigation from "../Navigation/Navigation";
+import { ThemeProvider } from "@mui/material/styles";
+import { darkTheme } from "../../styles/Theme";
+import { CssBaseline } from "@mui/material";
 
 interface IUIGeneratorProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const UIGenerator:React.FC<IUIGeneratorProps> = ({children}) => {
+const UIGenerator: React.FC<IUIGeneratorProps> = ({ children }) => {
   return (
-    <div>
-        <Navigation/>
-        {children}    
-    </div>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Navigation />
+      {children}
+    </ThemeProvider>
+  );
+};
 
-export default UIGenerator
+export default UIGenerator;
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#121212",
+      paper: "#424242",
+    },
+  },
+});
